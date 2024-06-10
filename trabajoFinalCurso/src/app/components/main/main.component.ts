e67474f22a9ab8a154b08373ef7a833f7e3ff969
lromero <lromero@gapd.es>
2024-06-10 15:17:40 +0200

    Home header with picture

--- trabajoFinalCurso/src/app/components/main/main.component.ts
+++ trabajoFinalCurso/src/app/components/main/main.component.ts
@@ -1,8 +1,10 @@
-import { Component, AfterViewInit, ViewChild} from '@angular/core';
+import { Component, AfterViewInit, ViewChild, OnInit} from '@angular/core';
 import { RouterOutlet,  RouterModule, RouterLink, ActivatedRoute } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { routes } from '../../app.routes.';
 import { appConfig } from '../../app.config';
+import { ItemsService } from '../../services/items.service';
+import { Item } from '../../models/item.model';
 
 
 
@@ -14,18 +16,30 @@ import { appConfig } from '../../app.config';
   styleUrl: './main.component.css',
   providers: [{provide: ActivatedRoute, useValue: routes}]
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
   public token: any;
   public cookie: any;
 
+
   public activeUser: any;
   constructor(loogin: LoginService) { 
+
 
     this.cookie = loogin;
     this.token = loogin.getToken();
     console.log(this.token);
+
+
   }
+
+  ngOnInit(): void {
+
+    
+  }
+
+
+  
 
   desconexion() {
     this.cookie.eliminarToken();
